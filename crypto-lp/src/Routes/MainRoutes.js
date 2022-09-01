07182fef2e672216586a671af3bba40c79d8352b
mansi <mansibavishi640@gmail.com>
2022-09-01 17:27:10 +0530

    1 SEP

--- crypto-lp/src/Routes/MainRoutes.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Connection = React.lazy(() => import("../User/connection/Connection"));
-
-const MainRoutes = [
-  { path: "/", component: <Connection /> },
-];
-
-export default MainRoutes;
